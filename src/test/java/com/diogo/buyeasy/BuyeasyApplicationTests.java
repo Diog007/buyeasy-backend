a603a7946e04f3d8472bb6c8912ff8fe15ee15c5
Diogo do Nascimento <diogo.mz123@gmail.com>
2024-01-04 17:19:32 -0300

    Inital Commit

---
+++ src/test/java/com/diogo/buyeasy/BuyeasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diogo.buyeasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyeasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
