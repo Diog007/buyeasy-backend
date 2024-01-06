6461d5c0e3ad3ad2646cf4cae4be3f525b00a9b2
Diogo do Nascimento <diogo.mz123@gmail.com>
2024-01-06 14:17:21 -0300

    Order, Instant, ISO 8601 - PART 2

---
+++ src/main/java/com/diogo/buyeasy/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.diogo.buyeasy.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.diogo.buyeasy.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+
+}
---
+++ src/main/java/com/diogo/buyeasy/services/OrderService.java
@@ -0,0 +1,30 @@
+package com.diogo.buyeasy.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.diogo.buyeasy.entities.Order;
+import com.diogo.buyeasy.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+	
+	
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+	
+	
+}
