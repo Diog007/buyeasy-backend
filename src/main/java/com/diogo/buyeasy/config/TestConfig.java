e37ff78422d2cd73a8c75c487ee09873f86bc61f
Diogo do Nascimento <diogo.mz123@gmail.com>
2024-01-08 12:42:11 -0300

    Payment, one-to-one association

--- src/main/java/com/diogo/buyeasy/config/TestConfig.java
+++ src/main/java/com/diogo/buyeasy/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.diogo.buyeasy.entities.Category;
 import com.diogo.buyeasy.entities.Order;
 import com.diogo.buyeasy.entities.OrderItem;
+import com.diogo.buyeasy.entities.Payment;
 import com.diogo.buyeasy.entities.Product;
 import com.diogo.buyeasy.entities.User;
 import com.diogo.buyeasy.entities.enums.OrderStatus;
@@ -81,7 +82,10 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
-	}
-	
-	
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
+		
+	}	
 }
