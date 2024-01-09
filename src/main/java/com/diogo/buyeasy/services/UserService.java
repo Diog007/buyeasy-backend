725e1f09114e5f858d7951a9df73601ac0f7b587
Diogo do Nascimento <diogo.mz123@gmail.com>
2024-01-09 13:01:10 -0300

    Exception handling - findById

--- src/main/java/com/diogo/buyeasy/services/UserService.java
+++ src/main/java/com/diogo/buyeasy/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.diogo.buyeasy.entities.User;
 import com.diogo.buyeasy.repositories.UserRepository;
+import com.diogo.buyeasy.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -22,7 +23,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
---
+++ src/main/java/com/diogo/buyeasy/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,15 @@
+package com.diogo.buyeasy.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id " + id);
+	}
+	
+	
+	
+	
+	
+
+}
